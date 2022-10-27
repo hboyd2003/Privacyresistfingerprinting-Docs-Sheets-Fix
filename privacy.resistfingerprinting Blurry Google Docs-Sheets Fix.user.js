9393b04fb85fb9c60d7f81f0fee8f9ac6f8645ca
H. Boyd <harrisonboyd400@gmail.com>
2022-10-27 19:39:39 +0000

    Add namespace URL

--- privacy.resistfingerprinting Blurry Google Docs-Sheets Fix.user.js
+++ privacy.resistfingerprinting Blurry Google Docs-Sheets Fix.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         privacy.resistfingerprinting Blurry Google Docs/Sheets Fix
-// @version      1.0
+// @version      1.1
 // @description  Fixes blurry font on Google Docs and Sheets. This issue is caused by resistfingerprinting forcing window.devicePixelRatio to be set to 1 no matter the actual value. WARNING! This overides the spoofed devicePixelRatio with the real value while in a Google Docs/Sheets document thus decreasing security.
 // @author       Prismo
 // @license      GPL-3.0-only
 // @supportURL   https://github.com/hboyd2003/Privacyresistfingerprinting-Docs-Sheets-Fix/issues
 // @downloadURL  https://raw.githubusercontent.com/hboyd2003/Privacyresistfingerprinting-Docs-Sheets-Fix/main/privacy.resistfingerprinting%20Blurry%20Google%20Docs-Sheets%20Fix.user.js
 // @updateURL    https://raw.githubusercontent.com/hboyd2003/Privacyresistfingerprinting-Docs-Sheets-Fix/main/privacy.resistfingerprinting%20Blurry%20Google%20Docs-Sheets%20Fix.user.js
+// @namespace    https://greasyfork.org/users/976025
 // @icon         https://raw.githubusercontent.com/hboyd2003/Privacyresistfingerprinting-Docs-Sheets-Fix/main/FixDocsIcon.png
 // @match        *://docs.google.com/document/d*
 // @match        *://docs.google.com/spreadsheets/d*
